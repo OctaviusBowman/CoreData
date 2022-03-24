da560ee6ac047558000c6429f5333f1fea88252f
Octavius Bowman <octaviusbowman@gmail.com>
2022-03-24 15:40:57 -0400

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Octavius Bowman on 3/24/22.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
