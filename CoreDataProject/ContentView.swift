7251a2952818496b4e51195d414d1f727f6f5f58
Octavius Bowman <octaviusbowman@gmail.com>
2022-03-30 18:27:20 -0400

    Core Data

--- CoreDataProject/ContentView.swift
+++ CoreDataProject/ContentView.swift
@@ -5,17 +5,38 @@
 //  Created by Octavius Bowman on 3/24/22.
 //
 
+import CoreData
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(sortDescriptors: [], predicate: nil) var ships: FetchedResults<Ship>
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    var body: some View {
+        VStack {
+            List(ships, id: \.self) { ship in
+                Text(ship.name ?? "Unknown name")
+            }
+
+            Button("Add Examples") {
+                let ship1 = Ship(context: moc)
+                ship1.name = "Enterprise"
+                ship1.universe = "Star Trek"
+
+                let ship2 = Ship(context: moc)
+                ship2.name = "Defiant"
+                ship2.universe = "Star Trek"
+
+                let ship3 = Ship(context: moc)
+                ship3.name = "Millennium Falcon"
+                ship3.universe = "Star Wars"
+
+                let ship4 = Ship(context: moc)
+                ship4.name = "Executor"
+                ship4.universe = "Star Wars"
+
+                try? moc.save()
+            }
+        }
     }
 }
